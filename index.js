fae3f1d96af6a51a1519b1cd805fed8900cb1ab9
Omar Qaqish <omqaqish@gmail.com>
2023-03-17 20:00:29 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 32;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random)
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod)
+
+const numbers = [2, 4, 8, 16, 20];
+const max = Math.max(...numbers);
+console.log(max)
